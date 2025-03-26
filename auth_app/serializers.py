4106788c547677f65125b58e6f129b7508f18586
Light <bluelight142@gmail.com>
2025-03-27 03:58:28 +1300

    added user login feature

--- auth_app/serializers.py
+++ auth_app/serializers.py
@@ -12,3 +12,8 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
